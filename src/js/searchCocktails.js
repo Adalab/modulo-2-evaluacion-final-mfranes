84483d2eb46351dd61c7282a130e52d74dbb4c1c
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-04-26 21:36:39 +0200

    create and article from js

--- src/js/searchCocktails.js
+++ src/js/searchCocktails.js
@@ -1,5 +1,15 @@
 console.log('search');
 const handleClickSearch = (event) => {
     console.log('handleClickSearch');
+    const listSection = document.querySelector('.js_listSection');
+    const anArticle = document.createElement('article');
+    const cocktailImage = document.createElement('span');
+    cocktailImage.textContent='image';
+    anArticle.appendChild(cocktailImage);
+    const cocktailName = document.createElement('h2');
+    cocktailName.textContent = 'Margarita';
+    anArticle.appendChild(cocktailName);
+    listSection.appendChild(anArticle);
+
 }
 btnSearch.addEventListener('click', handleClickSearch);
