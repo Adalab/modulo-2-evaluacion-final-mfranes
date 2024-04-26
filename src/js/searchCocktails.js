978f484ae2a09881eba3fb8af8a839dfc5e77bf0
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-04-26 21:58:22 +0200

    create a function for article creation

--- src/js/searchCocktails.js
+++ src/js/searchCocktails.js
@@ -1,15 +1,20 @@
 console.log('search');
 const handleClickSearch = (event) => {
     console.log('handleClickSearch');
-    const listSection = document.querySelector('.js_listSection');
-    const anArticle = document.createElement('article');
-    const cocktailImage = document.createElement('span');
-    cocktailImage.textContent='image';
-    anArticle.appendChild(cocktailImage);
-    const cocktailName = document.createElement('h2');
-    cocktailName.textContent = 'Margarita';
-    anArticle.appendChild(cocktailName);
-    listSection.appendChild(anArticle);
+    createArticle('urlImage', 'Cocktail1');
+    createArticle('urlImage2', 'Cocktail2');
 
 }
 btnSearch.addEventListener('click', handleClickSearch);
+
+const createArticle = (imageUrl, cocktailTitle) => {
+    const listSection = document.querySelector('.js_listSection');
+    const anArticle = document.createElement('article');
+    const cocktailImage = document.createElement('span');
+    cocktailImage.textContent= imageUrl;
+    anArticle.appendChild(cocktailImage);
+    const cocktailName = document.createElement('h2');
+    cocktailName.textContent = cocktailTitle;
+    anArticle.appendChild(cocktailName);
+    listSection.appendChild(anArticle);
+}
