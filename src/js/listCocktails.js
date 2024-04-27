098814ec2e9c0f9b7f9695edc848dd8697efffa9
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-04-27 18:45:22 +0200

    move funtion and add preventdefault

--- src/js/listCocktails.js
+++ src/js/listCocktails.js
@@ -1,1 +1,21 @@
 console.log('list');
+
+const createArticle = (imageUrl, cocktaillName) => {
+    const listSection = document.querySelector(".js_listSection");
+  
+    const ulListCocktail = document.createElement("ul");
+    ulListCocktail.classList.add('js_listSection__ul');
+    const listElement = document.createElement("li");
+    listSection.appendChild(ulListCocktail);
+    ulListCocktail.appendChild(listElement);
+  
+    const anArticle = document.createElement("article");
+    const cocktailImage = document.createElement("img");
+    cocktailImage.src = imageUrl;
+    cocktailImage.classList.add('js_listSection__cocktailThumbnail');
+    anArticle.appendChild(cocktailImage);
+    const cocktailTitle = document.createElement("h2");
+    cocktailTitle.textContent = cocktaillName;
+    anArticle.appendChild(cocktailTitle);
+    listElement.appendChild(anArticle);
+  };
--- src/js/searchCocktails.js
+++ src/js/searchCocktails.js
@@ -1,45 +1,28 @@
 console.log("search");
+
 const handleClickSearch = (event) => {
+  event.preventDefault();
   console.log("handleClickSearch");
-  
-  renderCocktails("margarita"); 
+
+  renderCocktails("margarita");
 };
+
 btnSearch.addEventListener("click", handleClickSearch);
-
-const createArticle = (imageUrl, cocktaillName) => {
-  const listSection = document.querySelector(".js_listSection");
-
-  const ulListCocktail = document.createElement("ul");
-  ulListCocktail.classList.add('js_listSection__ul');
-  const listElement = document.createElement("li");
-  listSection.appendChild(ulListCocktail);
-  ulListCocktail.appendChild(listElement);
-
-  const anArticle = document.createElement("article");
-  const cocktailImage = document.createElement("img");
-  cocktailImage.src = imageUrl;
-  cocktailImage.classList.add('js_listSection__cocktailThumbnail');
-  anArticle.appendChild(cocktailImage);
-  const cocktailTitle = document.createElement("h2");
-  cocktailTitle.textContent = cocktaillName;
-  anArticle.appendChild(cocktailTitle);
-  listElement.appendChild(anArticle);
-};
 
 const renderCocktails = (cocktaillName) => {
   const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktaillName}`;
 
- return fetch(url, {
-    method: "GET"
+  fetch(url, {
+    method: "GET",
   })
     .then((response) => response.json())
     .then((dataResponse) => {
       // console.log(dataResponse);
       for (const drink of dataResponse.drinks) {
-        const imageUrl =drink.strDrinkThumb;
+        const imageUrl = drink.strDrinkThumb;
         const drinkName = drink.strDrink;
-        
-        createArticle(imageUrl,drinkName);
+
+        createArticle(imageUrl, drinkName);
 
         // console.log(drink.strDrink);
       }
