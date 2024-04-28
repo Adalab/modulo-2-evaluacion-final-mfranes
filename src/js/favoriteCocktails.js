2e6b956c21eb77c014358944096c495c2a3a5eaf
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-04-28 15:16:08 +0200

    add icon to favorite cocktail

--- src/js/favoriteCocktails.js
+++ src/js/favoriteCocktails.js
@@ -1,24 +1,24 @@
 console.log('favorite');
 
-const favoriteCocktails = ()=> {
+const createFavoriteCocktail = (cocktailName) => {
+  const sectionFavorite = document.querySelector(".js_favoriteSection");
+  const favoriteUL = document.querySelectort(".js_ulFavorite");
 
-  const sectionFavorite = document.querySelector('.js_favoriteSection');
-  const favoriteUL = document.querySelectort('.js_ulFavorite');
-
-  const favoriteLi = document.createElement('li');
+  const favoriteLi = document.createElement("li");
   sectionFavorite.appendChild(favoriteUL);
   favoriteUL.appendChild(favoriteLi);
 
-  const favoriteArticle = document.createElement('article');
-  const favoriteImg = document.createElement('img');
-  const favoriteName = document.createElement('h2');
-  //favoriteName.textContent = favoriteCocktaillName;
+  const favoriteArticle = document.createElement("article");
+  const favoriteImg = document.createElement("img");
+  const favoriteName = document.createElement("h2");
+  favoriteName.textContent = cocktailName;
+  const deleteIcon = document.createElement("i");
+  deleteIcon.classList.add('fa fa-times-circle');
 
-  favoriteLi.appendChild(favoriteArticle);   
+  favoriteLi.appendChild(favoriteArticle);
   favoriteLi.appendChild(favoriteImg);
   favoriteLi.appendChild(favoriteName);
-  
-
-}
+  favoriteLi.appendChild(deleteIcon);
+};
 
 
