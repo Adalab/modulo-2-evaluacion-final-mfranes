4fa7d7fa7eb92a9ed923384679774d80020bb00d
María Francisca Espinoza <emariafrancisca@gmail.com>
2024-04-28 11:11:48 +0200

    create li and article  in favorite list

--- src/js/favoriteCocktails.js
+++ src/js/favoriteCocktails.js
@@ -1,1 +1,24 @@
 console.log('favorite');
+
+const favoriteCocktails = ()=> {
+
+  const sectionFavorite = document.querySelector('.js_favoriteSection');
+  const favoriteUL = document.querySelectort('.js_ulFavorite');
+
+  const favoriteLi = document.createElement('li');
+  sectionFavorite.appendChild(favoriteUL);
+  favoriteUL.appendChild(favoriteLi);
+
+  const favoriteArticle = document.createElement('article');
+  const favoriteImg = document.createElement('img');
+  const favoriteName = document.createElement('h2');
+  //favoriteName.textContent = favoriteCocktaillName;
+
+  favoriteLi.appendChild(favoriteArticle);   
+  favoriteLi.appendChild(favoriteImg);
+  favoriteLi.appendChild(favoriteName);
+  
+
+}
+
+
